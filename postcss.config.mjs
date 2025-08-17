84ba40b593024685b551a9988d9f692c3716b13a
danduke99 <72228945+danduke99@users.noreply.github.com>
2025-08-17 14:21:39 -0400

    Add files via upload

---
+++ postcss.config.mjs
@@ -0,0 +1,13 @@
+const config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        butler: ['Butler', 'serif'],
+        script: ['Script', 'cursive'],
+      },
+    },
+  },
+  plugins: ["@tailwindcss/postcss"],
+};
+
+export default config;
